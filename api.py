4a4b362bb3319bd8d1dd480a5bb623cbbd240f1e
ozero reka <rahmalva@icloud.com>
2024-09-10 22:32:05 +0300

    show new year picture

--- api.py
+++ api.py
@@ -8,7 +8,8 @@ from pprint import pprint
 
 nasa_url = 'https://api.nasa.gov/planetary/apod'
 params = {
-    'api_key': 'Fkj7ZEx6zWfRQeNpfcMjeXfYt7y39IrCr3URbq5w'
+    'api_key': 'Fkj7ZEx6zWfRQeNpfcMjeXfYt7y39IrCr3URbq5w',
+    'date': '2024-01-01'
 }
 response = requests.get(nasa_url, params=params)
 print(response.text)
@@ -21,3 +22,6 @@ image_url = response.json()['url']
 response = requests.get(image_url)
 with open('image.jpg', 'wb') as f:
     f.write(response.content)
+
+# 3 Просмотр картины 1 января ('date': '2024-01-01')
+
